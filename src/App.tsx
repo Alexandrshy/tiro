334abc0f223ecdca808829de07b284376f983846
Alexandrshy <alexandrshy@gmail.com>
2020-09-26 01:42:10 +0400

    fix: added useEffect for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { observer } from "mobx-react";
+import React, { useEffect } from "react";
 
 import { Content } from "@components/content";
 import { Header } from "@components/header";
+import { useStore } from "@stores/context";
 
-const App: React.FC = () => {
+const App: React.FC = observer(() => {
+  const store = useStore();
+
+  useEffect(() => {
+    store.previewCanvas.setCanvas("Dribbble");
+  });
+
   return (
     <>
       <Header />
       <Content />
     </>
   );
-};
+});
 
 export default App;
