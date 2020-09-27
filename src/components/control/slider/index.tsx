239041318653fa4ce55387c49c251192368b7b21
Alexandrshy <alexandrshy@gmail.com>
2020-09-27 22:46:47 +0400

    fix: fixed slider control

--- src/components/control/slider/index.tsx
+++ src/components/control/slider/index.tsx
@@ -1,32 +1,65 @@
-import { observer } from "mobx-react";
-import React from "react";
-
-import { useStore } from "@stores/context";
+import classNames from "classnames";
+import React, { useCallback } from "react";
 
 import style from "./style.module.css";
 
+export type PropsType = {
+  id: string;
+  isVisibleLabel?: boolean;
+  min?: number;
+  max?: number;
+  value: string;
+  className?: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
 /**
- * Stylized range slider
+ * Range slider field
+ * @param {string} props.id - Control id
+ * @param {boolean} props.isVisibleLabel - Is a visible label
+ * @param {number} props.min - Minimum value
+ * @param {number} props.max - Maximum value
+ * @param {string} props.value - Value
+ * @param {string} props.className - Additional className
+ * @param {string} props.value - Value
+ * @param {onChange} props.onChange - Method for handling input changes
  */
-export const Slider: React.FC = observer(() => {
-  const store = useStore();
+export const Slider: React.FC<PropsType> = ({
+  id,
+  isVisibleLabel = true,
+  min = 0,
+  max = 100,
+  value,
+  className,
+  onChange,
+  children,
+}) => {
+  const onChangeHandler = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => onChange(event),
+    [onChange]
+  );
 
   return (
     <div className={style.wrapper}>
       <div className={style.box}>
         <input
-          id="scale-slider"
+          id={id}
           type="range"
-          className={style.input}
-          value={store.previewScale.value}
-          min="10"
-          max="100"
-          onChange={(event) => store.previewScale.setScale(event)}
+          className={classNames(style.input, className)}
+          value={value}
+          min={min}
+          max={max}
+          onChange={onChangeHandler}
         />
-        <label htmlFor="scale-slider" className={style.label}>
-          Changing the scale of the preview
+        <label
+          htmlFor={id}
+          className={classNames({
+            "visually-hidden": !isVisibleLabel,
+          })}
+        >
+          {children}
         </label>
       </div>
     </div>
   );
-});
+};
