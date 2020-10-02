f7e429f74e493d378badb8125b65dbaaa962abc6
Alexandrshy <alexandrshy@gmail.com>
2020-10-03 00:23:00 +0400

    fix: addded click hander for loading img

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,43 @@
-import React from "react";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
+import { observer } from "mobx-react";
+import React, { useCallback } from "react";
 
 import { Button } from "@components/button";
 import { Logo } from "@components/logo";
+import { useStore } from "@stores/context";
 
 import style from "./style.module.css";
 
 /**
  * Header
  */
-export const Header: React.FC = () => {
+export const Header: React.FC = observer(() => {
+  const store = useStore();
+
+  const onDownload = useCallback(() => {
+    const element = document.getElementById("preview");
+
+    if (element) {
+      domtoimage
+        .toBlob(element, {
+          width: Number(store.previewSize.width),
+          height: Number(store.previewSize.height),
+          style: {
+            width: `${Number(store.previewSize.width)}px`,
+            height: `${Number(store.previewSize.height)}px`,
+            transform: "scale(1)",
+          },
+        })
+        .then((blob) => saveAs(blob, "tiro-preview.png"))
+        .catch((error) => console.error(error));
+    }
+  }, [store.previewSize.height, store.previewSize.width]);
+
   return (
     <header className={style.header}>
       <Logo />
-      <Button>Download</Button>
+      <Button onClick={onDownload}>Download</Button>
     </header>
   );
-};
+});
